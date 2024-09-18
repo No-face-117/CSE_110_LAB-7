ad375c6dd7f33f34c9bd43458dd1d8b94a4a3dae
No-face-117 <sayedmahamudalif@gmail.com>
2024-09-18 19:30:43 +0600

    Add files via upload

---
+++ As_7_Task_3_b.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class As_7_Task_3_b {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Enter N : ");
+        int ak = scan.nextInt();
+
+        int[] ar_1 = new int[ak];
+
+        for (int i = 0; i < ak; i++) {
+            System.out.println("Enter index " + i + " : ");
+            ar_1[i] = scan.nextInt();
+        }
+
+        System.out.println();
+
+        for (int i = 0; i < ak / 2; i++) {
+            int temp = ar_1[i];  
+            ar_1[i] = ar_1[ak - i - 1];  
+            ar_1[ak - i - 1] = temp; 
+        }
+
+        System.out.println("Reversed array : ");
+        
+        for (int l = 0; l < ak; l++) {
+            System.out.print(ar_1[l] + " ");
+        }
+
+        scan.close();
+    }
+}
