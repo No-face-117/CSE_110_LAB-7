7f3543b4f927f47dbad2a9db9bd877228e67c141
No-face-117 <sayedmahamudalif@gmail.com>
2024-09-18 19:22:20 +0600

    reverse an array with another

---
+++ As_7_Task_3_a.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class As_7_Task_3_a {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Enter N: ");
+        int ak = scan.nextInt();
+
+        int[] ar_1 = new int[ak];
+        int[] ar_2 = new int[ak];
+
+        for (int i = 0; i < ak; i++) {
+            System.out.println("Enter value for index " + i + ": ");
+            ar_1[i] = scan.nextInt();
+        }
+
+        System.out.println();
+
+        for (int j = 0; j < ak; j++) {
+            ar_2[ak - j - 1] = ar_1[j];
+        }
+
+        System.out.println("Reversed array:");
+        for (int l = 0; l < ak; l++) {
+            System.out.print(ar_2[l] + " ");
+        }
+
+        scan.close();
+    }
+}
